ac67da9f1cd9c011d67bb5f74a8bc18079eac3c3
GudochkinK <goodok-01@hotmail.com>
2020-02-02 18:31:03 +0300

    Тестовые данные

--- MailSender.lib/Entities/Server.cs
+++ MailSender.lib/Entities/Server.cs
@@ -12,7 +12,7 @@ namespace MailSender.lib.Entities
         public string Name { get; set; }
         public string Address { get; set; }
         public int Port { get; set; }
-        public bool UseSsl { get; set; }
+        public bool UseSsl { get; set; } = true;
         public string Login { get; set; }
         public string Password { get; set; }
     }
