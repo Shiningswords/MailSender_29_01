c326be4c239b0904798588c1130edc6bf110ee35
GudochkinK <goodok-01@hotmail.com>
2020-02-13 22:04:59 +0300

    Пример работы с пулом потоков

--- Tests/Threading/Program.cs
+++ Tests/Threading/Program.cs
@@ -7,30 +7,9 @@ namespace Threading
     {
         static void Main(string[] args)
         {
-            Thread.CurrentThread.Name = "Главный поток";
-            CheckThread(Thread.CurrentThread);
-            var first_thread = new Thread(FirstThreadMethod);
-            
-            first_thread.Name = "Фоновый поток";
-            first_thread.Priority = ThreadPriority.BelowNormal;
-            first_thread.IsBackground = true;
-            first_thread.Start();
-            CheckThread(first_thread);
-
-            Console.WriteLine("Главный поток завершился");
-            Console.ReadLine();
-        }
-        private static void FirstThreadMethod()
-        {
-            while (true)
-            {
-                Console.Title = DateTime.Now.ToString();
-                Thread.Sleep(100);
-            }
-        }
-        private static void CheckThread(Thread thread)
-        {
-            Console.WriteLine("Поток[{0}] {1} - {2} фоновый",thread.Name,thread.ManagedThreadId,thread.IsBackground ? "" : "не ");
+            //ThreadTests.Start();
+            ThreadPoolTests.Start();
+            Console.WriteLine("Приложение должно быть закрыто");
         }
     }
 }
