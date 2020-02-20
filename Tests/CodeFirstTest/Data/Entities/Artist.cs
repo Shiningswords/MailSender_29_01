74e55bd1e3edacc9d54593a95aba40fa32182dcd
GudochkinK <goodok-01@hotmail.com>
2020-02-20 22:25:24 +0300

    Сущности БД CodeFirst

---
+++ Tests/CodeFirstTest/Data/Entities/Artist.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstTest.Data.Entities
+{
+    //[Table("Artist")]
+    public class Artist
+    {
+        //[Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime? Birthday { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; }// = new List<Song>();
+
+        public virtual ICollection<Distributors> Distributors { get; set; }
+    }
+}
+}
---
+++ Tests/CodeFirstTest/Data/Entities/Distributors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirstTest.Data.Entities
+{
+    public class Distributors
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public virtual ICollection<Artist> Artists { get; set; }
+    }
+}
