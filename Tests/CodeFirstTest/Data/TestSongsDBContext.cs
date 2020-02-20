d3a1fa444b28461bdb6b96767104e732e11605be
GudochkinK <goodok-01@hotmail.com>
2020-02-20 22:56:42 +0300

    Code First

---
+++ Tests/CodeFirstTest/Data/TestSongsDBContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstTest.Data.Entities;
+//using CodeFirstTest.Migrations;
+
+namespace CodeFirstTest.Data
+{
+    public class TestSongsDBContext : DbContext
+    {
+        //static TestSongsDBContext() => System.Data.Entity.Database.SetInitializer(new DropCreateDatabaseAlways<TestSongsDBContext>());
+        //static TestSongsDBContext() => System.Data.Entity.Database.SetInitializer(new CreateDatabaseIfNotExists<TestSongsDBContext>());
+        //static TestSongsDBContext() => System.Data.Entity.Database.SetInitializer(new DropCreateDatabaseIfModelChanges<TestSongsDBContext>());
+        //static TestSongsDBContext() => Database.SetInitializer(new MigrateDatabaseToLatestVersion<TestSongsDBContext, Configuration>());
+
+
+        public DbSet<Artist> Artists { get; set; }
+
+        public DbSet<Song> Songs { get; set; }
+
+        public DbSet<Distributors> Distributors { get; set; }
+
+
+        public TestSongsDBContext() : this("name=DefaultConnection") { }
+
+        public TestSongsDBContext(string ConnectionString) : base(ConnectionString) { }
+    }
+}
--- Tests/CodeFirstTest/Program.cs
+++ Tests/CodeFirstTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CodeFirstTest.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,19 @@ namespace CodeFirstTest
     {
         static void Main(string[] args)
         {
+            using (var db = new TestSongsDBContext())
+            {
+                db.Database.Log = Console.WriteLine;
+
+                foreach (var artist in db.Artists.Where(a => a.Birthday < DateTime.Now))
+                    foreach (var artist_song in artist.Songs)
+                        Console.WriteLine(artist_song.Title);
+
+                var values = db.Artists.Select(artist => new { Name = artist.Name }).ToArray();
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
